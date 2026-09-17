ec04766e0948161f2f2daa60329328674560c476
maksymkapitaniuk <maksym.kapitaniuk.dev@gmail.com>
2026-09-17 17:04:49 +0300

    add container to navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { Button } from '../ui/Button';
 import logo from '../../assets/logo.svg';
@@ -18,35 +19,42 @@ export function Navbar() {
 
   return (
     <AppBar position="static" color="default">
-      <Toolbar disableGutters sx={{ px: 4 }}>
-        <Box sx={{ flexGrow: 1, display: 'flex', gap: 4 }}>
-          <Link to="/vehicles">
-            <Box
-              component="img"
-              src={logo}
-              alt="Logo"
-              sx={{ cursor: 'pointer' }}
-            />
-          </Link>
-          <Button component={Link} to="/users" variant="text" color="clean">
-            Users
-          </Button>
+      <Container>
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: 'flex', gap: 4 }}>
+            <Link to="/vehicles">
+              <Box
+                component="img"
+                src={logo}
+                alt="Logo"
+                sx={{ cursor: 'pointer' }}
+              />
+            </Link>
+            <Button component={Link} to="/users" variant="text" color="clean">
+              Users
+            </Button>
 
-          <Button component={Link} to="/vehicles" variant="text" color="clean">
-            Vehicles
-          </Button>
-        </Box>
-        {isLoggedIn && (
-          <Button
-            variant="text"
-            color="clean"
-            onClick={handleLogout}
-            sx={{ flexGrow: 0 }}
-          >
-            Logout
-          </Button>
-        )}
-      </Toolbar>
+            <Button
+              component={Link}
+              to="/vehicles"
+              variant="text"
+              color="clean"
+            >
+              Vehicles
+            </Button>
+          </Box>
+          {isLoggedIn && (
+            <Button
+              variant="text"
+              color="clean"
+              onClick={handleLogout}
+              sx={{ flexGrow: 0 }}
+            >
+              Logout
+            </Button>
+          )}
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
